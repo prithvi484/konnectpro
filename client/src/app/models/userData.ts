9f83958c45b2b4f6caf0cd3fdbe277ba57d68854
sharath <sharath.k@transneuron.com>
2018-03-23 15:22:49 +0530

    deleted old ui

--- client/src/app/models/userData.ts
+++
@@ -1,9 +0,0 @@
-export class UserData {
-    firstName: string = '';
-    lastName: string = '';
-    email: string = '';
-    mobileNumber: string = '';
-    role: string = '';
-    aadhar: string = '';
-    empolyeeId: string = '';
-}
